d4b2691e85e1b2b97bbfdd30f02f0cc7269fad56
jacopo-wisee <jacopo@wisee.it>
2023-12-16 12:58:08 +0100

    dutch auction is working

--- auction/src/auction.py
+++ auction/src/auction.py
@@ -12,56 +12,45 @@ config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 llm_config = {"config_list": config_list, "seed": 42, "timeout": 60}
 
 # Loading prompts
-with open("./src/prompts/auctioneer/auctioneer_context.txt", "r") as f_in:
-    auctioneer_context = f_in.read()
-
 with open("./src/prompts/auctioneer/auctioneer_prompt.txt", "r") as f_in:
     auctioneer_prompt = f_in.read()
-
-with open("./src/prompts/participant/participant_context.txt", "r") as f_in:
-    participant_context = f_in.read()
 
 with open("./src/prompts/participant/participant_prompt.txt", "r") as f_in:
     participant_prompt = f_in.read()
 
-with open("./src/prompts/proxy/proxy_prompt.txt", "r") as f_in:
-    proxy_prompt = f_in.read()
+with open("./src/prompts/participant/participant_prompt2.txt", "r") as f_in:
+    participant_prompt2 = f_in.read()
 
 # Initialize agents
 user_proxy = UserProxyAgent(
     name="user_proxy",
     human_input_mode="NEVER",
-    max_consecutive_auto_reply=10,
-    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    llm_config=llm_config,
-    system_message=proxy_prompt,
+    max_consecutive_auto_reply=4,
+    llm_config=llm_config
 )
 
 auctioneer = AssistantAgent(
     name="auctioneer",
     llm_config=llm_config,
-    system_message=auctioneer_context.format(
-        asking_price=1000, percentage="5%", reserve_price=700
-    )
-    + auctioneer_prompt,
+    system_message=auctioneer_prompt.format(asking_price=1000, percentage="5%", reserve_price=700),
 )
 
 participant1 = AssistantAgent(
     name="participant1",
     llm_config=llm_config,
-    system_message=participant_context.format(price=920) + participant_prompt,
+    system_message=participant_prompt.format(price=920)
 )
 
 participant2 = AssistantAgent(
     name="participant2",
     llm_config=llm_config,
-    system_message=participant_context.format(price=900) + participant_prompt,
+    system_message=participant_prompt2.format(price=900)
 )
 
 """participant3 = AssistantAgent(
     name="participant3",
     llm_config=llm_config,
-    system_message=participant_context.format(price=890) + participant_prompt,
+    system_message=participant_prompt.format(price=890),
 )"""
 
 # Instantiate the groupchat
